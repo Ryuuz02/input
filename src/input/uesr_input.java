ab6772384bdc24deead58710f0d0d5749e34ece0
Ryuuz02 <ryuuz02@outlook.com>
2023-03-14 17:21:25 -0500

    first commit

---
+++ src/input/uesr_input.java
@@ -0,0 +1,13 @@
+package input;
+import java.util.Scanner;
+
+public class uesr_input {
+
+	public static void main(String[] args) {
+		Scanner user_input = new Scanner(System.in);
+		System.out.println(user_input.nextLine());
+		user_input.close();
+
+	}
+
+}
